f7f8db92621f62ef1d290c97c226b109dac1e900
Damian Lupa <damian.lupa88@gmail.com>
2020-01-07 23:04:24 +0100

    slider

--- src/components/SliderSelectTypeOfPets.js
+++ src/components/SliderSelectTypeOfPets.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
-// import test from '../images/test2.png'
+import { Carousel } from 'react-bootstrap'
 import '../styles/Slider.css'
 import dog from '../images/dog.png'
 import cat from '../images/cat.png'
 import rabbit from '../images/rabbit.png'
+import next from "../images/icons/angle-right.svg"
+import prev from "../images/icons/angle-left.svg"
 
 
 class Slider extends Component {
@@ -12,49 +14,115 @@ class Slider extends Component {
 
     return (
 
-      <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+      // <Carousel>
 
+      //   <Carousel.Item>
+      //     <img
+      //       className="d-block w-100 abc"
+      //       src={dog}
+      //       alt="dog"
+      //     />
+      //     <Carousel.Caption>
+      //       <h5>Dog</h5>
+
+      //     </Carousel.Caption>
+      //   </Carousel.Item>
+
+      //   <Carousel.Item>
+      //     <img
+      //       className="d-block w-100 abc"
+      //       src={cat}
+      //       alt="cat"
+      //     />
+
+      //     <Carousel.Caption>
+      //       <h5>cat</h5>
+
+      //     </Carousel.Caption>
+      //   </Carousel.Item>
+
+      //   <Carousel.Item>
+      //     <img
+      //       className="d-block w-100 abc"
+      //       src={rabbit}
+      //       alt="rabbit"
+      //     />
+
+      //     <Carousel.Caption>
+      //       <h5>rabbit</h5>
+
+      //     </Carousel.Caption>
+      //   </Carousel.Item>
+      // </Carousel>
+
+      <div class="carousel slide">
         <ol class="carousel-indicators">
-          <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
-          <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
-          <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+          <li></li>
+          <li class="active"></li>
+          <li></li>
         </ol>
-
         <div class="carousel-inner">
-          <div class="carousel-item active">
-            <img src={cat} class="d-block w-100 abc" alt="..." />
-            <div class="carousel-caption d-none d-md-block">
-              <h5>Cat</h5>
-              {/* <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p> */}
-            </div>
-          </div>
           <div class="carousel-item">
-            <img src={dog} class="d-block w-100 abc" alt="..." />
-            <div class="carousel-caption d-none d-md-block">
+            <img
+              class="d-block w-100 abc"
+              src={dog}
+              alt="dog"
+            />
+
+            <div class="carousel-caption">
               <h5>Dog</h5>
-              {/* <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p> */}
+            </div>
+          </div>
+
+          <div class="active carousel-item">
+            <img
+              class="d-block w-100 abc"
+              src={cat}
+              alt="cat"
+            />
+            <div class="carousel-caption">
+              <h5>cat</h5>
             </div>
           </div>
           <div class="carousel-item">
-            <img src={rabbit} class="d-block w-100 abc" alt="..." />
-            <div class="carousel-caption d-none d-md-block">
-              <h5>Rabbit</h5>
-              {/* <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p> */}
+            <img
+              class="d-block w-100 abc"
+              src={rabbit}
+              alt="rabbit"
+            />
+            <div class="carousel-caption">
+              <h5>rabbit</h5>
             </div>
           </div>
         </div>
 
-        <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="sr-only">Previous</span>
+        <a class="carousel-control-prev" role="button" href="#">
+          {/* <span aria-hidden="true" class="carousel-control-prev-icon">
+          </span>
+          <span class="sr-only">Previous</span> */}
+            <img
+            src={prev}
+            alt="next"
+            class="w-50"
+          />
         </a>
 
-        <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="sr-only">Next</span>
+        <a class="carousel-control-next" role="button" href="#">
+          {/* <span aria-hidden="true" class="carousel-control-next-icon">
+          </span> */}
+          <img
+            src={next}
+            alt="next"
+            class="w-50"
+          />
+          {/* <span class="sr-only">Next</span> */}
         </a>
 
       </div>
+
+
+
+
     )
   }
 }
