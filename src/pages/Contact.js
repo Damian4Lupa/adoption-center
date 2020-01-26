fc5a5abbac33308d31ae68f63c053265b2603754
Damian Lupa <damian.lupa88@gmail.com>
2020-01-26 22:46:13 +0100

    Contact component

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
-import { Container, Row, Col } from 'react-bootstrap'
+import { Container, Row, Col, Form, Button } from 'react-bootstrap'
 import '../styles/Pages.css'
 
 import home from '../images/website/home.svg'
 import phone from '../images/website/phone.svg'
+import envelope from '../images/website/envelope.svg'
+import skype from '../images/website/skype.svg'
 
 class Contact extends Component {
     state = {}
@@ -16,7 +18,7 @@ class Contact extends Component {
 
                 <Row>
 
-                    <Col sm={3} className="mt-5 adres">
+                    <Col sm={3} className="adress">
 
                         <img src={home} alt="home" height="25" />
                         <p>Adopt Center Foundation</p>
@@ -28,34 +30,62 @@ class Contact extends Component {
                         <img src={phone} alt="phone" height="25" />
                         <p>(541) 754-3010</p>
 
+                        <br />
+
+                        <img src={envelope} alt="envelope" height="25" />
+                        <p>info@adoptcenter.com</p>
+
+                        <br />
+
+                        <img src={skype} alt="skype" height="25" />
+                        <p>AdoptionCenter</p>
+
                     </Col>
-
-
-
                     <Col sm={9} className="mt-5">
 
-
                         <div id="map" />
-
-
-
-
 
                     </Col>
                 </Row>
 
-                <div className="mt-5">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt quidem amet illum architecto reiciendis impedit magnam minus odit adipisci sit deserunt praesentium, repellat necessitatibus earum hic officia maxime est eaque!</p>
+                <h4 className="text-center mt-5 mb-2">Write to us</h4>
 
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt quidem amet illum architecto reiciendis impedit magnam minus odit adipisci sit deserunt praesentium, repellat necessitatibus earum hic officia maxime est eaque!</p>
+                <Form className="my-4">
 
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt quidem amet illum architecto reiciendis impedit magnam minus odit adipisci sit deserunt praesentium, repellat necessitatibus earum hic officia maxime est eaque!</p>
+                    <Form.Row>
+                        <Col xs={6}>
+                            <Form.Group controlId="email">
+                                <Form.Control type="email" placeholder="email" />
+                            </Form.Group>
 
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt quidem amet illum architecto reiciendis impedit magnam minus odit adipisci sit deserunt praesentium, repellat necessitatibus earum hic officia maxime est eaque!</p>
-                </div>
 
+                            <Form.Group controlId="subject">
 
+                                <Form.Control type="text" placeholder="subject" />
+                            </Form.Group>
 
+                        </Col>
+                        <Col xs={6}>
+
+                            <Form.Group controlId="message">
+
+                                <Form.Control placeholder="message" as="textarea" rows="3" />
+
+                            </Form.Group>
+
+                        </Col>
+                    </Form.Row>
+                    <Form.Row>
+                        <Col sm={10}></Col>
+
+                        <Col sm={2}>
+                            <Form.Group controlId="button" className="mb-5 mt-2">
+                                <Button variant="outline-primary" className="mr-3">Clear</Button>
+                                <Button variant="primary" type="submit" >Submit</Button>
+                            </Form.Group>
+                        </Col>
+                    </Form.Row>
+                </Form>
 
             </Container>
         );
