cadf92ec68485db0d6e7b26b7913f3cf53f04360
Damian Lupa <damian.lupa88@gmail.com>
2020-02-08 22:51:24 +0100

    Counter draft

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import '../styles/Counter.css'
+import $ from 'jquery'
 
 import handsHelping from '../images/website/hands-helping.svg'
 import paw from '../images/website/paw.svg'
@@ -7,8 +8,114 @@ import home from '../images/website/home.svg'
 import donate from '../images/website/donate.svg'
 
 class Counter extends Component {
-    state = {}
+    state = {
+        startCounter: false,
+        endCounter: false,
+
+        petsInDatabase: 0,
+        adoptedPets: 0,
+        donors: 0,
+        volunteers: 0
+    }
+
+
+    counter_petsInDatabase = 0
+
+    counter = () => {
+
+        let petsInDatabase = 0
+        // let adoptedPets = 0
+        // let donors = 0
+        // let volunteers = 0
+
+        let petsInDatabaseMax = 1200
+        // let adoptedPetsMax = 1277
+        // let donorsMax = 869
+        // let volunteersMax = 76
+
+        if (this.counter_petsInDatabase <= petsInDatabaseMax) {
+            this.counter_petsInDatabase = petsInDatabase + 10
+            petsInDatabase = petsInDatabase + 10
+            console.log(petsInDatabase)
+
+            this.setState({
+                petsInDatabase
+            })
+        }
+
+
+    }
+
+    checkScrollValue = () => {
+
+        // $(document).ready(function () {
+        //     let początek = $('.counter').offset().top
+        //     let scrollValue = $(this).scrollTop();
+        //     // console.log(scrollValue)
+
+        //     if (scrollValue > początek) {
+        //         console.log("działa")
+        //         // this.counter()
+        //     }
+        // });
+
+        let startCounter = $('.counter').offset().top
+        let scrollValue = $(window).scrollTop();
+
+        // console.log("startCounter:", startCounter, "||  scrollValue:", scrollValue)
+
+        if (scrollValue >= startCounter) {
+            console.log("startCounter: true")
+            this.setState({
+                startCounter: true
+            })
+        }
+
+
+    }
+
+
+
+
+
+
+
+
+
     render() {
+
+        const { startCounter, petsInDatabase, adoptedPets, donors, volunteers } = this.state
+
+        // this.checkScrollValue()
+
+        let checkScrollValue = setInterval(() => {
+            this.checkScrollValue()
+            // this.counter()
+        }, 500);
+
+        if (startCounter) {
+            clearInterval(checkScrollValue)
+            let startCounter = setInterval(() => {
+                this.counter()
+                // this.checkScrollValue()
+            }, 500);
+
+
+
+
+            // console.log("clearInterval(checkScrollValue)")
+        }
+
+
+
+
+
+
+        // setTimeout(() => {
+        //     clearInterval(interval)
+        //     console.log("koniec działania")
+        // }, 2500);
+
         return (
             <div className="ts_counter p-0">
                 <div className="container-fluid">
@@ -17,7 +124,7 @@ class Counter extends Component {
 
                             <img src={paw} alt="paw" height="40" />
                             <div className="facts-num">
-                                <span className="counter">1200</span>
+                                <span className="counter">{petsInDatabase}</span>
                             </div>
                             <h3>Pets in the database</h3>
                         </div>
