606fcad8006afeaf6a246256fe8ac4501c6aca4b
Damian Lupa <damian.lupa88@gmail.com>
2020-02-09 17:21:46 +0100

    Counter component ended

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import '../styles/Counter.css'
 import $ from 'jquery'
+import CountUp from 'react-countup';
 
 import handsHelping from '../images/website/hands-helping.svg'
 import paw from '../images/website/paw.svg'
@@ -9,112 +10,37 @@ import donate from '../images/website/donate.svg'
 
 class Counter extends Component {
     state = {
-        startCounter: false,
-        endCounter: false,
-
-        petsInDatabase: 0,
-        adoptedPets: 0,
-        donors: 0,
-        volunteers: 0
-    }
-
-
-    counter_petsInDatabase = 0
-
-    counter = () => {
-
-        let petsInDatabase = 0
-        // let adoptedPets = 0
-        // let donors = 0
-        // let volunteers = 0
-
-        let petsInDatabaseMax = 1200
-        // let adoptedPetsMax = 1277
-        // let donorsMax = 869
-        // let volunteersMax = 76
-
-        if (this.counter_petsInDatabase <= petsInDatabaseMax) {
-            this.counter_petsInDatabase = petsInDatabase + 10
-            petsInDatabase = petsInDatabase + 10
-            console.log(petsInDatabase)
-
-            this.setState({
-                petsInDatabase
-            })
-        }
-
-
+        startCounter: false
     }
 
     checkScrollValue = () => {
 
-        // $(document).ready(function () {
-        //     let początek = $('.counter').offset().top
-        //     let scrollValue = $(this).scrollTop();
-        //     // console.log(scrollValue)
+        let startCounter = $('.ts_counter').offset().top - 600
+        let scrollValue = $(document).scrollTop();
+        let endCounter = $('.ts_counter').offset().top + 100
 
-        //     if (scrollValue > początek) {
-        //         console.log("działa")
-        //         // this.counter()
-        //     }
-        // });
+        if (scrollValue >= startCounter && scrollValue <= endCounter) {
 
-        let startCounter = $('.counter').offset().top
-        let scrollValue = $(window).scrollTop();
-
-        // console.log("startCounter:", startCounter, "||  scrollValue:", scrollValue)
-
-        if (scrollValue >= startCounter) {
-            console.log("startCounter: true")
             this.setState({
                 startCounter: true
             })
         }
-
-
     }
-
-
-
-
-
-
-
-
 
     render() {
 
-        const { startCounter, petsInDatabase, adoptedPets, donors, volunteers } = this.state
+        const { startCounter } = this.state
 
-        // this.checkScrollValue()
-
-        let checkScrollValue = setInterval(() => {
-            this.checkScrollValue()
-            // this.counter()
-        }, 500);
-
-        if (startCounter) {
-            clearInterval(checkScrollValue)
-            let startCounter = setInterval(() => {
-                this.counter()
-                // this.checkScrollValue()
+        if (!startCounter) {
+            setInterval(() => {
+                this.checkScrollValue()
             }, 500);
-
-
-
-
-            // console.log("clearInterval(checkScrollValue)")
         }
 
-
-
-
-
-
-        // setTimeout(() => {
-        //     clearInterval(interval)
-        //     console.log("koniec działania")
-        // }, 2500);
+        let petsInDatabase = startCounter ? <CountUp end={1278} /> : 0
+        let adoptedPets = startCounter ? <CountUp end={5987} /> : 0
+        let donors = startCounter ? <CountUp end={786} /> : 0
+        let volunteers = startCounter ? <CountUp end={76} /> : 0
 
         return (
             <div className="ts_counter p-0">
@@ -124,7 +50,8 @@ class Counter extends Component {
 
                             <img src={paw} alt="paw" height="40" />
                             <div className="facts-num">
-                                <span className="counter">{petsInDatabase}</span>
+                                <span className="counter">  {petsInDatabase}</span>
+
                             </div>
                             <h3>Pets in the database</h3>
                         </div>
@@ -133,7 +60,7 @@ class Counter extends Component {
 
                             <img src={home} alt="home" height="40" />
                             <div className="facts-num">
-                                <span className="counter">1277</span>
+                                <span className="counter">{adoptedPets}</span>
                             </div>
                             <h3>Adopted pets</h3>
                         </div>
@@ -142,7 +69,7 @@ class Counter extends Component {
 
                             <img src={donate} alt="donate" height="40" />
                             <div className="facts-num">
-                                <span className="counter">869</span>
+                                <span className="counter">{donors}</span>
                             </div>
                             <h3>Donors</h3>
                         </div>
@@ -151,7 +78,7 @@ class Counter extends Component {
 
                             <img src={handsHelping} alt="handsHelping" height="40" />
                             <div className="facts-num">
-                                <span className="counter">76</span>
+                                <span className="counter">{volunteers}</span>
                             </div>
                             <h3>Volunteers</h3>
                         </div>
