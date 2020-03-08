d3c051b892e5f8ce63d3f4cb4baa14eb18a65bd1
Damian Lupa <damian.lupa88@gmail.com>
2020-03-08 22:27:43 +0100

    AdoptionInfo draft, GetInvolved component ended

--- src/pages/AdoptionInfo.js
+++ src/pages/AdoptionInfo.js
@@ -1,24 +1,192 @@
 import React from 'react';
-import { Container } from 'react-bootstrap'
+import { Container, Accordion, Card } from 'react-bootstrap'
+
+import HowWeHelp from '../images/website/AdoptionInfo/HowWeHelp.jpg'
 
 const AdoptionInfo = () => {
 
     return (
-        <Container>
+        <>
+            <Container className="adoptionInfo my-5">
 
-            <p className="my-5">Get Involved</p>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse, iste assumenda magni atque unde quisquam, aut debitis nulla cupiditate sapiente ut nisi aspernatur repellat natus. Optio assumenda accusamus sunt vel. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse, iste assumenda magni atque unde quisquam, aut debitis nulla cupiditate sapiente ut nisi aspernatur repellat natus. Optio assumenda accusamus sunt vel.</p>
+
+                <Accordion defaultActiveKey="0" className="my-5">
+                    <Card>
+                        <Accordion.Toggle as={Card.Header} eventKey="0">
+                            <b>10 Things To Consider Before Adopting A Pet</b>
+                        </Accordion.Toggle>
+                        <Accordion.Collapse eventKey="0">
+                            <Card.Body>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias soluta deserunt provident non delectus voluptatem, obcaecati incidunt commodi. Ut porro fuga similique molestiae dicta deleniti omnis, at quod. Dolorem, aliquid!Sunt maiores cumque soluta. Dolor nesciunt minus minima saepe ut debitis odit aperiam obcaecati sunt sit! Soluta, excepturi laborum aspernatur, non quisquam odio mollitia delectus nisi, repellat in placeat blanditiis.</p>
+                            </Card.Body>
+                        </Accordion.Collapse>
+                    </Card>
+                    <Card>
+                        <Accordion.Toggle as={Card.Header} eventKey="1">
+                            <b>Why there is a Fee For Adoption</b>
+                        </Accordion.Toggle>
+                        <Accordion.Collapse eventKey="1">
+                            <Card.Body>Hello! I'm another body</Card.Body>
+                        </Accordion.Collapse>
+                    </Card>
+                    <Card>
+                        <Accordion.Toggle as={Card.Header} eventKey="2">
+                            <b>Shelter, SPCA, humane society or rescue – what’s the difference?</b>
+                        </Accordion.Toggle>
+                        <Accordion.Collapse eventKey="2">
+                            <Card.Body>Hello! I'm another body</Card.Body>
+                        </Accordion.Collapse>
+                    </Card>
+                    <Card>
+                        <Accordion.Toggle as={Card.Header} eventKey="3">
+                            <b>Chemistry: picking the right pet for you</b>
+                        </Accordion.Toggle>
+                        <Accordion.Collapse eventKey="3">
+                            <Card.Body>Hello! I'm another body</Card.Body>
+                        </Accordion.Collapse>
+                    </Card>
+                    <Card>
+                        <Accordion.Toggle as={Card.Header} eventKey="4">
+                            <b>How to avoid picking the wrong pet</b>
+                        </Accordion.Toggle>
+                        <Accordion.Collapse eventKey="4">
+                            <Card.Body>Hello! I'm another body</Card.Body>
+                        </Accordion.Collapse>
+                    </Card>
+                    <Card>
+                        <Accordion.Toggle as={Card.Header} eventKey="5">
+                            <b>How to pick a pet if you have allergies</b>
+                        </Accordion.Toggle>
+                        <Accordion.Collapse eventKey="5">
+                            <Card.Body>Hello! I'm another body</Card.Body>
+                        </Accordion.Collapse>
+                    </Card>
+                    <Card>
+                        <Accordion.Toggle as={Card.Header} eventKey="6">
+                            <b>The Advantages of Adopting Pets In Pairs</b>
+                        </Accordion.Toggle>
+                        <Accordion.Collapse eventKey="6">
+                            <Card.Body>Hello! I'm another body</Card.Body>
+                        </Accordion.Collapse>
+                    </Card>
+                    <Card>
+                        <Accordion.Toggle as={Card.Header} eventKey="7">
+                            <b>Boy vs girl – what’s the difference?</b>
+                        </Accordion.Toggle>
+                        <Accordion.Collapse eventKey="7">
+                            <Card.Body>Hello! I'm another body</Card.Body>
+                        </Accordion.Collapse>
+                    </Card>
+                    <Card>
+                        <Accordion.Toggle as={Card.Header} eventKey="8">
+                            <b>Does coat color determine a cat’s personality?</b>
+                        </Accordion.Toggle>
+                        <Accordion.Collapse eventKey="8">
+                            <Card.Body>Hello! I'm another body</Card.Body>
+                        </Accordion.Collapse>
+                    </Card>
+                    <Card>
+                        <Accordion.Toggle as={Card.Header} eventKey="9">
+                            <b>Adopt a Senior Pet!</b>
+                        </Accordion.Toggle>
+                        <Accordion.Collapse eventKey="9">
+                            <Card.Body>Hello! I'm another body</Card.Body>
+                        </Accordion.Collapse>
+                    </Card>
+                    <Card>
+                        <Accordion.Toggle as={Card.Header} eventKey="10">
+                            <b>Disabled pets can be amazing!</b>
+                        </Accordion.Toggle>
+                        <Accordion.Collapse eventKey="10">
+                            <Card.Body>Hello! I'm another body</Card.Body>
+                        </Accordion.Collapse>
+                    </Card>
+                    <Card>
+                        <Accordion.Toggle as={Card.Header} eventKey="11">
+                            <b>Average Cost of Owning a pets</b>
+                        </Accordion.Toggle>
+                        <Accordion.Collapse eventKey="11">
+                            <Card.Body>Hello! I'm another body</Card.Body>
+                        </Accordion.Collapse>
+                    </Card>
+                    <Card>
+                        <Accordion.Toggle as={Card.Header} eventKey="12">
+                            <b>How to prepare for your pet’s vet costs</b>
+                        </Accordion.Toggle>
+                        <Accordion.Collapse eventKey="12">
+                            <Card.Body>Hello! I'm another body</Card.Body>
+                        </Accordion.Collapse>
+                    </Card>
+                    <Card>
+                        <Accordion.Toggle as={Card.Header} eventKey="13">
+                            <b>Vet Q&A: Are pets for adoption healthy?</b>
+                        </Accordion.Toggle>
+                        <Accordion.Collapse eventKey="13">
+                            <Card.Body>Hello! I'm another body</Card.Body>
+                        </Accordion.Collapse>
+                    </Card>
+                    <Card>
+                        <Accordion.Toggle as={Card.Header} eventKey="14">
+                            <b>Renting with pets</b>
+                        </Accordion.Toggle>
+                        <Accordion.Collapse eventKey="14">
+                            <Card.Body>Hello! I'm another body</Card.Body>
+                        </Accordion.Collapse>
+                    </Card>
+                    <Card>
+                        <Accordion.Toggle as={Card.Header} eventKey="15">
+                            <b>Giving Pets as Gifts</b>
+                        </Accordion.Toggle>
+                        <Accordion.Collapse eventKey="15">
+                            <Card.Body>Hello! I'm another body</Card.Body>
+                        </Accordion.Collapse>
+                    </Card>
+                    <Card>
+                        <Accordion.Toggle as={Card.Header} eventKey="16">
+                            <b>Study Shows Babies Exposed to Pets at Home are Healthier</b>
+                        </Accordion.Toggle>
+                        <Accordion.Collapse eventKey="16">
+                            <Card.Body>Hello! I'm another body</Card.Body>
+                        </Accordion.Collapse>
+                    </Card>
+                    <Card>
+                        <Accordion.Toggle as={Card.Header} eventKey="17">
+                            <b>Pet Adoption Teaches Good Parenting</b>
+                        </Accordion.Toggle>
+                        <Accordion.Collapse eventKey="17">
+                            <Card.Body>Hello! I'm another body</Card.Body>
+                        </Accordion.Collapse>
+                    </Card>
+                    <Card>
+                        <Accordion.Toggle as={Card.Header} eventKey="18">
+                            <b>10 Reasons Pets Are Good For Kids</b>
+                        </Accordion.Toggle>
+                        <Accordion.Collapse eventKey="18">
+                            <Card.Body>Hello! I'm another body</Card.Body>
+                        </Accordion.Collapse>
+                    </Card>
+
+                </Accordion>
 
 
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt quidem amet illum architecto reiciendis impedit magnam minus odit adipisci sit deserunt praesentium, repellat necessitatibus earum hic officia maxime est eaque!</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt quidem amet illum architecto reiciendis impedit magnam minus odit adipisci sit deserunt praesentium, repellat necessitatibus earum hic officia maxime est eaque!</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt quidem amet illum architecto reiciendis impedit magnam minus odit adipisci sit deserunt praesentium, repellat necessitatibus earum hic officia maxime est eaque!</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt quidem amet illum architecto reiciendis impedit magnam minus odit adipisci sit deserunt praesentium, repellat necessitatibus earum hic officia maxime est eaque!</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt quidem amet illum architecto reiciendis impedit magnam minus odit adipisci sit deserunt praesentium, repellat necessitatibus earum hic officia maxime est eaque!</p>
 
 
+            </Container>
+            <img src={HowWeHelp} class="img-fluid" alt="Responsive image"></img>
+            <Container className="my-5">
 
-        </Container>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt quidem amet illum architecto reiciendis impedit magnam minus odit adipisci sit deserunt praesentium, repellat necessitatibus earum hic officia maxime est eaque!Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt quidem amet illum architecto reiciendis impedit magnam minus odit adipisci sit deserunt praesentium, repellat necessitatibus earum hic officia maxime est eaque!</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt quidem amet illum architecto reiciendis impedit magnam minus odit adipisci sit deserunt praesentium, repellat necessitatibus earum hic officia maxime est eaque!Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt quidem amet illum architecto reiciendis impedit magnam minus odit adipisci sit deserunt praesentium, repellat necessitatibus earum hic officia maxime est eaque!</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt quidem amet illum architecto reiciendis impedit magnam minus odit adipisci sit deserunt praesentium, repellat necessitatibus earum hic officia maxime est eaque!</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt quidem amet illum architecto reiciendis impedit magnam minus odit adipisci sit deserunt praesentium, repellat necessitatibus earum hic officia maxime est eaque!Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt quidem amet illum architecto reiciendis impedit magnam minus odit adipisci sit deserunt praesentium, repellat necessitatibus earum hic officia maxime est eaque!</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt quidem amet illum architecto reiciendis impedit magnam minus odit adipisci sit deserunt praesentium, repellat necessitatibus earum hic officia maxime est eaque!</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt quidem amet illum architecto reiciendis impedit magnam minus odit adipisci sit deserunt praesentium, repellat necessitatibus earum hic officia maxime est eaque!Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt quidem amet illum architecto reiciendis impedit magnam minus odit adipisci sit deserunt praesentium, repellat necessitatibus earum hic officia maxime est eaque!</p>
+
+            </Container>
+        </>
     );
 }
+
 
 export default AdoptionInfo;
--- src/pages/GetInvolved.js
+++ src/pages/GetInvolved.js
@@ -1,23 +1,76 @@
 import React from 'react';
-import { Container } from 'react-bootstrap'
+import { Container, Row, Col } from 'react-bootstrap'
+import '../styles/Pages.css'
+import pets1 from '../images/website/GetInvolved/pets1.jpg'
+import pets2 from '../images/website/GetInvolved/pets2.jpg'
+import pets3 from '../images/website/GetInvolved/pets3.jpg'
+import pets4 from '../images/website/GetInvolved/pets4.jpg'
+import pets5 from '../images/website/GetInvolved/pets5.jpg'
+import pets6 from '../images/website/GetInvolved/pets6.jpg'
+import pets7 from '../images/website/GetInvolved/pets7.jpg'
+import pets8 from '../images/website/GetInvolved/pets8.jpg'
+import pets9 from '../images/website/GetInvolved/pets9.jpg'
+
 
 const GetInvolved = () => {
 
-
     return (
-        <Container>
+        <Container className="getInvolved">
+            <Row className="my-5">
+                <Col>
+                    <img src={pets6} alt="..." width="300" class="img-thumbnail" />
+                    <h4>nsjfndsjnjdsnfjn</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est ad delectus fugiat eius accusantium, rerum quas blanditiis suscipit enim. Porro totam alias quod iusto vel esse harum temporibus, excepturi qui.</p>
+                </Col>
+                <Col>
+                    <img src={pets2} alt="..." width="300" class="img-thumbnail" />
+                    <h4>nsjfndsjnjdsnfjn</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est ad delectus fugiat eius accusantium, rerum quas blanditiis suscipit enim. Porro totam alias quod iusto vel esse harum temporibus, excepturi qui.</p>
+                </Col>
+                <Col>
+                    <img src={pets8} alt="..." width="300" class="img-thumbnail" />
+                    <h4>nsjfndsjnjdsnfjn</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est ad delectus fugiat eius accusantium, rerum quas blanditiis suscipit enim. Porro totam alias quod iusto vel esse harum temporibus, excepturi qui.</p>
+                </Col>
+            </Row>
+            <Row className="my-5">
+                <Col>
+                    <img src={pets4} alt="..." width="300" class="img-thumbnail" />
+                    <h4>nsjfndsjnjdsnfjn</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est ad delectus fugiat eius accusantium, rerum quas blanditiis suscipit enim. Porro totam alias quod iusto vel esse harum temporibus, excepturi qui.</p>
+                </Col>
+                <Col>
+                    <img src={pets5} alt="..." width="300" class="img-thumbnail" />
+                    <h4>nsjfndsjnjdsnfjn</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est ad delectus fugiat eius accusantium, rerum quas blanditiis suscipit enim. Porro totam alias quod iusto vel esse harum temporibus, excepturi qui.</p>
+                </Col>
+                <Col>
+                    <img src={pets7} alt="..." width="300" class="img-thumbnail" />
+                    <h4>nsjfndsjnjdsnfjn</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est ad delectus fugiat eius accusantium, rerum quas blanditiis suscipit enim. Porro totam alias quod iusto vel esse harum temporibus, excepturi qui.</p>
+                </Col>
+            </Row>
+            <Row className="my-5">
+                <Col>
+                    <img src={pets3} alt="..." width="300" class="img-thumbnail" />
+                    <h4>nsjfndsjnjdsnfjn</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est ad delectus fugiat eius accusantium, rerum quas blanditiis suscipit enim. Porro totam alias quod iusto vel esse harum temporibus, excepturi qui.</p>
+                </Col>
+                <Col>
+                    <img src={pets1} alt="..." width="300" class="img-thumbnail" />
+                    <h4>nsjfndsjnjdsnfjn</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est ad delectus fugiat eius accusantium, rerum quas blanditiis suscipit enim. Porro totam alias quod iusto vel esse harum temporibus, excepturi qui.</p>
+                </Col>
+                <Col>
+                    <img src={pets9} alt="..." width="300" class="img-thumbnail" />
+                    <h4>nsjfndsjnjdsnfjn</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est ad delectus fugiat eius accusantium, rerum quas blanditiis suscipit enim. Porro totam alias quod iusto vel esse harum temporibus, excepturi qui.</p>
+                </Col>
+            </Row>
 
-        <p className="my-5">Get Involved</p>
-
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt quidem amet illum architecto reiciendis impedit magnam minus odit adipisci sit deserunt praesentium, repellat necessitatibus earum hic officia maxime est eaque!</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt quidem amet illum architecto reiciendis impedit magnam minus odit adipisci sit deserunt praesentium, repellat necessitatibus earum hic officia maxime est eaque!</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt quidem amet illum architecto reiciendis impedit magnam minus odit adipisci sit deserunt praesentium, repellat necessitatibus earum hic officia maxime est eaque!</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt quidem amet illum architecto reiciendis impedit magnam minus odit adipisci sit deserunt praesentium, repellat necessitatibus earum hic officia maxime est eaque!</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt quidem amet illum architecto reiciendis impedit magnam minus odit adipisci sit deserunt praesentium, repellat necessitatibus earum hic officia maxime est eaque!</p>
 
 
-
-    </Container>
+        </Container>
 
     );
 }
